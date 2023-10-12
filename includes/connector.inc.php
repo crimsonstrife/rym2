5eb16202fff08c6a6e0185820179b53a228f28aa
Patrick Barnhardt <contact@patrickbarnhardt.info>
2023-10-12 15:12:30 -0400

    Update connector.inc.php

--- includes/connector.inc.php
+++ includes/connector.inc.php
@@ -16,8 +16,10 @@
 declare(strict_types=1); // Forces PHP to adhere to strict typing, if types do not match an error is thrown.
 
 /* This function will connect to the MySQL database and return the connection object using the provided credentials */
-function connectToDatabase(string $host, string $username, string $password, string $database, int $port): mysqli
+function connectToDatabase(string $host, string $username, string $password, string $database, string $port): mysqli
 {
+    //convert port to int
+    $port = (int) $port;
     /* Setup the connection to MySQL */
     $mysqli = new mysqli($host, $username, $password, $database, $port);
 
@@ -29,4 +31,36 @@ function connectToDatabase(string $host, string $username, string $password, str
 
     /* Return the connection object */
     return $mysqli;
+}
+
+/* This function will close the connection to the MySQL database */
+function closeDatabaseConnection(mysqli $mysqli): void
+{
+    /* Close the connection to the database */
+    $mysqli->close();
+}
+
+/* This function just tests the connection to the MySQL database, but only returns a boolean */
+function testDatabaseConnection(string $host, string $username, string $password, string $database, string $port): bool
+{
+    // convert port to int
+    $port = (int) $port;
+
+    /* Try to connect to the database */
+    try {
+        $mysqli = new mysqli($host, $username, $password, $database, $port);
+        //catch any errors and return false
+    } catch (Exception $e) {
+        // Log the error
+        error_log("Failed to connect to the database: (" . $mysqli->connect_errno . ")" . $mysqli->connect_error);
+        // if the database is unknown, return false
+        if ($mysqli->connect_errno == 1049) {
+            return false;
+        }
+        return false;
+    }
+
+    /* Close the connection to the database */
+    $mysqli->close();
+    return true;
 }
