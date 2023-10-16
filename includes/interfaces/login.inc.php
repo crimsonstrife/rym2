e0ec15896102594dba11d84fc5569c6f15744022
Patrick Barnhardt <contact@patrickbarnhardt.info>
2023-10-16 15:38:36 -0400

    Create login.inc.php

---
+++ includes/interfaces/login.inc.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Login interface file for the College Recruitment Application
+ * Contains all the login functions for users.
+ *
+ * Author:  Patrick Barnhardt
+ * Author Email: pbarnh1@wgu.edu
+ * Date:   10/16/2023
+ *
+ * @package RYM2
+ * Filename: login.inc.php
+ * @version 1.0.0
+ * @requires PHP 7.2.5+
+ */
+
+declare(strict_types=1); // Forces PHP to adhere to strict typing, if types do not match an error is thrown.
+
+/* Include the base application config file */
+
+require_once(__DIR__ . '/../../config/app.php');
+/* Include the database config file */
+require_once(__DIR__ . '/../../config/database.php');
+// include the database connector file
+require_once(BASEPATH . '/includes/connector.inc.php');
+
+interface Login
+{
+    public function login($username, $password);
+    public function logout();
+}
