eeaa9c3c1b6df6e93f5c2e0194866477f70b7333
Patrick Barnhardt <contact@patrickbarnhardt.info>
2023-10-16 15:38:23 -0400

    Update database.php

--- config/database.php
+++ config/database.php
@@ -21,16 +21,33 @@ require_once(__DIR__ . '/app.php');
 // include the database connector file
 require_once(BASEPATH . '/includes/connector.inc.php');
 
-/* Get ENV variables, if they are not set or do not meet requirements, throw an exception */
-$dotenv->required(['DB_HOST', 'DB_PORT', 'DB_DATABASE', 'DB_USERNAME', 'DB_PASSWORD'])->notEmpty();
-$dotenv->required('DB_PORT')->isInteger();
+if (file_exists(BASEPATH . '/.env')) {
+    /* Use the phpdotenv package to read the .env file */
+    $dotenv = Dotenv\Dotenv::createImmutable(BASEPATH);
+    $dotenv->safeLoad(); // Load the .env file if it exists.
 
-/* Define the database constants */
-define('DB_HOST', $_ENV['DB_HOST']); // Define the DB_HOST constant, this is the host of the database.
-define('DB_PORT', $_ENV['DB_PORT']); // Define the DB_PORT constant, this is the port of the database.
-define('DB_DATABASE', $_ENV['DB_DATABASE']); // Define the DB_DATABASE constant, this is the name of the database.
-define('DB_USERNAME', $_ENV['DB_USERNAME']); // Define the DB_USERNAME constant, this is the username of the database.
-define('DB_PASSWORD', $_ENV['DB_PASSWORD']); // Define the DB_PASSWORD constant, this is the password of the database.
+    /* Get ENV variables, if they are not set or do not meet requirements, throw an exception */
+    $dotenv->required(['DB_HOST', 'DB_PORT', 'DB_DATABASE', 'DB_USERNAME', 'DB_PASSWORD'])->notEmpty();
+    $dotenv->required('DB_PORT')->isInteger();
+
+    /* Define the database constants */
+    define('DB_HOST', $_ENV['DB_HOST']); // Define the DB_HOST constant, this is the host of the database.
+    define('DB_PORT', $_ENV['DB_PORT']); // Define the DB_PORT constant, this is the port of the database.
+    define('DB_DATABASE', $_ENV['DB_DATABASE']); // Define the DB_DATABASE constant, this is the name of the database.
+    define('DB_USERNAME', $_ENV['DB_USERNAME']); // Define the DB_USERNAME constant, this is the username of the database.
+    define('DB_PASSWORD', $_ENV['DB_PASSWORD']); // Define the DB_PASSWORD constant, this is the password of the database.
+} else {
+    /*load the .env.example file if the .env file does not exist */
+    $dotenv = Dotenv\Dotenv::createImmutable(BASEPATH, '.env.example');
+    $dotenv->safeLoad();
+
+    /* Define the database constants */
+    define('DB_HOST', $_ENV['DB_HOST']); // Define the DB_HOST constant, this is the host of the database.
+    define('DB_PORT', $_ENV['DB_PORT']); // Define the DB_PORT constant, this is the port of the database.
+    define('DB_DATABASE', $_ENV['DB_DATABASE']); // Define the DB_DATABASE constant, this is the name of the database.
+    define('DB_USERNAME', $_ENV['DB_USERNAME']); // Define the DB_USERNAME constant, this is the username of the database.
+    define('DB_PASSWORD', $_ENV['DB_PASSWORD']); // Define the DB_PASSWORD constant, this is the password of the database.
+}
 
 /* Attempt to connect to the MySQL database */
 try {
