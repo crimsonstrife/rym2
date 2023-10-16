e097e9ed99b52bfdf840ee4c8df6a6056fba059a
Patrick Barnhardt <contact@patrickbarnhardt.info>
2023-10-16 15:44:16 -0400

    Create students.inc.php

---
+++ includes/classes/students.inc.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Students Class file for the College Recruitment Application
+ * Contains all the functions for the Student Class and handles all the student related tasks with the database.
+ *
+ * Author:  Patrick Barnhardt
+ * Author Email: pbarnh1@wgu.edu
+ * Date:   10/16/2023
+ *
+ * @package RYM2
+ * Filename: students.inc.php
+ * @version 1.0.0
+ * @requires PHP 7.2.5+
+ */
+
+declare(strict_types=1); // Forces PHP to adhere to strict typing, if types do not match an error is thrown.
+
+/* Include the base application config file */
+
+require_once(__DIR__ . '/../../config/app.php');
+/* Include the database config file */
+require_once(__DIR__ . '/../../config/database.php');
+// include the database connector file
+require_once(BASEPATH . '/includes/connector.inc.php');
+
+class Students
+{
+}
