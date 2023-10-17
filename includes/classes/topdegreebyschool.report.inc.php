da04d20228772f7c323ff9daf4bbb1be1f82e3f2
Patrick Barnhardt <contact@patrickbarnhardt.info>
2023-10-17 13:37:42 -0400

    Create topdegreebyschool.report.inc.php

---
+++ includes/classes/topdegreebyschool.report.inc.php
@@ -0,0 +1,20 @@
+<?php
+
+
+class TopDegreeBySchoolReport extends Report
+{
+    public function getReport(): array
+    {
+        return array();
+    }
+
+    public function getReportById(int $id): array
+    {
+        return array();
+    }
+
+    public function findReport(string $search): array
+    {
+        return array();
+    }
+}
