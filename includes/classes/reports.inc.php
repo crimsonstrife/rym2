99f3b04faafd9cb38ed195bb5f5e0dacf8086df1
Patrick Barnhardt <contact@patrickbarnhardt.info>
2023-10-17 13:37:39 -0400

    Update reports.inc.php

--- includes/classes/reports.inc.php
+++ includes/classes/reports.inc.php
@@ -2,4 +2,9 @@
 
 abstract class Report
 {
+    public abstract function getReport(): array;
+
+    public abstract function getReportById(int $id): array;
+
+    public abstract function findReport(string $search): array;
 }
