cad0b46e46820e746e849e937b63293aaf7f4db7
Patrick Barnhardt <contact@patrickbarnhardt.info>
2023-10-19 08:45:58 -0400

    Create footer.php

---
+++ public/pages/footer.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * This is the default footer for pages of the College Recruitment Application
+ * Students will be able to enter their information and have it sent to the database.
+ * Uses the student class to create a new student object
+ *
+ * Author:  Patrick Barnhardt
+ * Author Email: pbarnh1@wgu.edu
+ * Date:   10/18/2023
+ *
+ * @package RYM2
+ * Filename: footer.php
+ * @version 1.0.0
+ * @requires PHP 7.2.5+
+ * @requires Bootstrap 5.3.2+
+ * @requires Font Awesome 6.4.2+
+ * @requires jQuery 3.7.1+
+ */
+/* Setup HTML for page footer */
+?>
+<footer>
+</footer>
+<!-- Bootstrap JavaScript -->
+<script src="<?php echo getLibraryPath(); ?>bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- Font Awesome -->
+<script src="<?php echo getLibraryPath(); ?>fontawesome/js/all.min.js"></script>
+<!-- jQuery -->
+<script src="<?php echo getLibraryPath(); ?>jquery/jquery.min.js"></script>
+
+</html>
