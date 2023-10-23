10b87d01eb7571f3c548919422d5bb34cf946c0f
Patrick Barnhardt <contact@patrickbarnhardt.info>
2023-10-23 15:20:08 -0400

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,2 +1,17 @@
 <?php
-include_once(__DIR__ . '/pages/landing_content.php');
+//check the url for an event slug
+if (isset($_GET['event'])) {
+    $event_slug = $_GET['event'];
+    $event = $db->getEventBySlug($event_slug);
+    if ($event) {
+        //if the event is found in the database, show the event page
+        include_once(__DIR__ . '/pages/event_content.php');
+    } else {
+        //if the event is not found in the database, show the 404 page
+        include_once(__DIR__ . '/pages/404.php');
+    }
+    return;
+} else {
+    //if no event slug is set, show the generic landing page
+    include_once(__DIR__ . '/pages/landing_content.php');
+}
