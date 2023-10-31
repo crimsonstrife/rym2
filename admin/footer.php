d1c9ee93725e2138c25eebc7a05a8721afc47159
Patrick Barnhardt <contact@patrickbarnhardt.info>
2023-10-31 14:45:09 -0400

    Create footer.php

---
+++ admin/footer.php
@@ -0,0 +1,18 @@
+<?php ?>
+<footer class="py-4 bg-light mt-auto">
+    <div class="container-fluid px-4">
+        <div class="d-flex align-items-center justify-content-between small">
+            <div class="text-muted">Copyright &copy; Patrick Barnhardt 2023</div>
+            <div>
+                <a href="#">Privacy Policy</a>
+                &middot;
+                <a href="#">Terms &amp; Conditions</a>
+            </div>
+        </div>
+    </div>
+    <?php echo includeFooter(); ?>
+</footer>
+</div>
+</body>
+
+</html>
