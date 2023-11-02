f0b33cb07a6c733f6474903e44205b18633a29ea
Patrick Barnhardt <contact@patrickbarnhardt.info>
2023-11-02 13:32:54 -0400

    Update header.php

--- admin/header.php
+++ admin/header.php
@@ -19,10 +19,12 @@ require_once(__DIR__ . '../../vendor/autoload.php');
 
 <body class="nav-fixed">
     <nav class="top-nav navbar navbar-expand navbar-dark bg-dark">
+        <!-- Sidebar Toggle-->
+        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!">
+            <i class="fas fa-bars"></i>
+        </button>
         <!-- Navbar Brand-->
         <a class="navbar-brand ps-3" href="<?php echo APP_URL ?>"><?php echo htmlspecialchars(APP_NAME) ?></a>
-        <!-- Sidebar Toggle-->
-        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
         <!-- Navbar Search-->
         <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
             <div class="input-group">
