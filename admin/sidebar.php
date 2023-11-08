dff8f57f5b3985f27d3fedf7c3c5ca497ddd4513
Patrick Barnhardt <contact@patrickbarnhardt.info>
2023-11-08 15:43:44 -0500

    Update sidebar.php

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -9,30 +9,41 @@
                     <div class="sb-nav-link-icon"><i class="fa-solid fa-gauge"></i></div>
                     Dashboard
                 </a>
-                <a class="nav-link" href="<?php echo APP_URL . '/admin/events.php' ?>">
+                <a class="nav-link" href="<?php echo APP_URL . '/admin/dashboard.php?view=events' ?>">
                     <div class="sb-nav-link-icon"><i class="fa-solid fa-calendar-day"></i></div>
                     Events
                 </a>
-                <a class="nav-link" href="<?php echo APP_URL . '/admin/schools.php' ?>">
+                <a class="nav-link" href="<?php echo APP_URL . '/admin/dashboard.php?view=schools' ?>">
                     <div class="sb-nav-link-icon"><i class="fa-solid fa-school"></i></div>
                     Schools
                 </a>
-                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts"
+                    aria-expanded="false" aria-controls="collapseLayouts">
                     <div class="sb-nav-link-icon"><i class="fa-solid fa-graduation-cap"></i></div>
                     Degree Programs
                     <div class="side-nav-collapse-arrow"><i class="fa-solid fa-chevron-down"></i></div>
                 </a>
-                <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne"
+                    data-bs-parent="#sidenavAccordion">
                     <nav class="side-nav-menu-nested nav">
-                        <a class="nav-link" href="<?php echo APP_URL . '/admin/majors.php' ?>">Majors</a>
-                        <a class="nav-link" href="<?php echo APP_URL . '/admin/degrees.php' ?>">Degrees</a>
+                        <a class="nav-link" href="<?php echo APP_URL . '/admin/dashboard.php?view=majors' ?>">Majors</a>
+                        <a class="nav-link"
+                            href="<?php echo APP_URL . '/admin/dashboard.php?view=degrees' ?>">Degrees</a>
                     </nav>
                 </div>
             </div>
         </div>
         <div class="side-nav-footer">
             <div class="small">Logged in as:</div>
-            <?php echo htmlspecialchars($_SESSION["username"]); ?>
+            <?php
+            //check if the user is logged in, using the session variable we set on login if it exists
+            if (isset($_SESSION['username'])) {
+                echo htmlspecialchars($_SESSION["username"]);
+            } else {
+                //if the session variable does not exist, use alternative means to ID the user
+
+            }
+            ?>
         </div>
     </nav>
 </div>
