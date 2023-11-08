284262ff0a834e2fe68f9d66f9de1ec25dd46618
Patrick Barnhardt <contact@patrickbarnhardt.info>
2023-11-08 15:43:59 -0500

    Update constants.php

--- includes/constants.php
+++ includes/constants.php
@@ -1,6 +1,15 @@
 <?php
+
+/**
+ * define the job types for labels
+ */
 define('JOBTYPES', array(
     array("value" => "FULL", "label" => "Full Time"),
     array("value" => "PART", "label" => "Part Time"),
     array("value" => "INTERN", "label" => "Internship")
 ));
+
+/**
+ * Define constant for date formats
+ */
+define('DATE_FORMAT', 'm/d/Y');
