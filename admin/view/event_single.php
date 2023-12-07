ac3a0de61add1e7dcdd2390cd3cb1af52d94745b
Patrick Barnhardt <contact@patrickbarnhardt.info>
2023-12-07 14:07:02 -0500

    Update event_single.php

--- admin/view/event_single.php
+++ admin/view/event_single.php
@@ -79,13 +79,13 @@ $event_id = $_GET['id'];
                                 <?php
                                 $qrCodeData = APP_URL . '/index.php?event=' . $event->getEventSlug($event_id);
                                 $qrCodeOptions = new QROptions;
-                                $qrCodeOptions->version = 10;
-                                $qrCodeOptions->outputType = QRCode::OUTPUT_IMAGE_JPG;
+                                $qrCodeOptions->version = 7;
+                                $qrCodeOptions->outputType = QROutputInterface::GDIMAGE_PNG;
                                 $qrCodeOptions->scale = 20;
-                                $qrCodeOptions->outputBase64 = false;
+                                $qrCodeOptions->outputBase64 = true;
                                 $qrCode = (new QRCode($qrCodeOptions))->render($qrCodeData); // per the documentation, https://php-qrcode.readthedocs.io/en/main/Usage/Quickstart.html
-
                                 //output the QRCode JPG
+                                header('Content-type: image/png');
                                 echo '<img src="' . $qrCode . '" alt="QRCode" style="max-width: 200px; max-height: auto;">';
                                 ?>
                             </div>
