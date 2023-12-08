e9d8b0791b7482a3975a198130bf5a2ee8af591a
Patrick Barnhardt <contact@patrickbarnhardt.info>
2023-12-08 13:45:27 -0500

    Update degrees_content.php

--- admin/degrees_content.php
+++ admin/degrees_content.php
@@ -7,5 +7,58 @@ if (!defined('ISVALIDUSER')) {
 <!-- main content -->
 <div id="layout_content">
     <main>
+        <?php
+        if (isset($_GET['degree'])) {
+            switch ($_GET['degree']) {
+                case 'list':
+                    include_once('view/degree_list.php');
+                    break;
+                case 'add':
+                    if (isset($_GET['action'])) {
+                        switch ($_GET['action']) {
+                            case 'create':
+                                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                                    include_once('editor/actions/degree/create.php');
+                                } else {
+                                    include_once('editor/degree_edit.php');
+                                }
+                                break;
+                            default:
+                                include_once('view/degree_list.php');
+                                break;
+                        }
+                    } else {
+                        include_once('view/degree_list.php');
+                    }
+                    break;
+                case 'edit':
+                    if (isset($_GET['action'])) {
+                        switch ($_GET['action']) {
+                            case 'edit':
+                                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                                    include_once('editor/actions/degree/modify.php');
+                                } else {
+                                    include_once('editor/degree_edit.php');
+                                }
+                                break;
+                            default:
+                                include_once('view/degree_list.php');
+                                break;
+                        }
+                    } else {
+                        include_once('view/degree_list.php');
+                    }
+                    break;
+                case 'single':
+                    include_once('view/degree_list.php');
+                    break;
+                default:
+                    include_once('view/degree_list.php');
+                    break;
+            }
+        } else {
+            include_once('view/degree_list.php');
+        }
+        ?>
     </main>
 </div>
