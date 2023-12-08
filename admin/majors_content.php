ead248ddf884b7a70d7b7c628f52593322fe1e12
Patrick Barnhardt <contact@patrickbarnhardt.info>
2023-12-08 13:45:31 -0500

    Update majors_content.php

--- admin/majors_content.php
+++ admin/majors_content.php
@@ -7,5 +7,58 @@ if (!defined('ISVALIDUSER')) {
 <!-- main content -->
 <div id="layout_content">
     <main>
+        <?php
+        if (isset($_GET['major'])) {
+            switch ($_GET['major']) {
+                case 'list':
+                    include_once('view/major_list.php');
+                    break;
+                case 'add':
+                    if (isset($_GET['action'])) {
+                        switch ($_GET['action']) {
+                            case 'create':
+                                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                                    include_once('editor/actions/major/create.php');
+                                } else {
+                                    include_once('editor/major_edit.php');
+                                }
+                                break;
+                            default:
+                                include_once('view/major_list.php');
+                                break;
+                        }
+                    } else {
+                        include_once('view/major_list.php');
+                    }
+                    break;
+                case 'edit':
+                    if (isset($_GET['action'])) {
+                        switch ($_GET['action']) {
+                            case 'edit':
+                                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                                    include_once('editor/actions/major/modify.php');
+                                } else {
+                                    include_once('editor/major_edit.php');
+                                }
+                                break;
+                            default:
+                                include_once('view/major_list.php');
+                                break;
+                        }
+                    } else {
+                        include_once('view/major_list.php');
+                    }
+                    break;
+                case 'single':
+                    include_once('view/major_list.php');
+                    break;
+                default:
+                    include_once('view/major_list.php');
+                    break;
+            }
+        } else {
+            include_once('view/major_list.php');
+        }
+        ?>
     </main>
 </div>
