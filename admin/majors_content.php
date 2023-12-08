791065523fe7663de7555b250581ebdd42ea8ca8
Patrick Barnhardt <contact@patrickbarnhardt.info>
2023-12-08 13:36:40 -0500

    Create majors_content.php

---
+++ admin/majors_content.php
@@ -0,0 +1,11 @@
+<?php
+//Prevent direct access to this file by checking if the constant ISVALIDUSER is defined.
+if (!defined('ISVALIDUSER')) {
+    die('Error: Invalid request');
+} // idea from https://stackoverflow.com/a/409515 (user UnkwnTech)
+?>
+<!-- main content -->
+<div id="layout_content">
+    <main>
+    </main>
+</div>
