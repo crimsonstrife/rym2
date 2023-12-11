edf4ad0da99529ac12816bb8a16ec7d853cbd91c
Patrick Barnhardt <contact@patrickbarnhardt.info>
2023-12-11 10:57:11 -0500

    Create jobs_content.php

---
+++ admin/jobs_content.php
@@ -0,0 +1,64 @@
+<?php
+//Prevent direct access to this file by checking if the constant ISVALIDUSER is defined.
+if (!defined('ISVALIDUSER')) {
+    die('Error: Invalid request');
+} // idea from https://stackoverflow.com/a/409515 (user UnkwnTech)
+?>
+<!-- main content -->
+<div id="layout_content">
+    <main>
+        <?php
+        if (isset($_GET['job'])) {
+            switch ($_GET['job']) {
+                case 'list':
+                    include_once('view/job_list.php');
+                    break;
+                case 'add':
+                    if (isset($_GET['action'])) {
+                        switch ($_GET['action']) {
+                            case 'create':
+                                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                                    include_once('editor/actions/job/create.php');
+                                } else {
+                                    include_once('editor/job_edit.php');
+                                }
+                                break;
+                            default:
+                                include_once('view/job_list.php');
+                                break;
+                        }
+                    } else {
+                        include_once('view/job_list.php');
+                    }
+                    break;
+                case 'edit':
+                    if (isset($_GET['action'])) {
+                        switch ($_GET['action']) {
+                            case 'edit':
+                                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                                    include_once('editor/actions/job/modify.php');
+                                } else {
+                                    include_once('editor/job_edit.php');
+                                }
+                                break;
+                            default:
+                                include_once('view/job_list.php');
+                                break;
+                        }
+                    } else {
+                        include_once('view/job_list.php');
+                    }
+                    break;
+                case 'single':
+                    include_once('view/job_list.php');
+                    break;
+                default:
+                    include_once('view/job_list.php');
+                    break;
+            }
+        } else {
+            include_once('view/job_list.php');
+        }
+        ?>
+    </main>
+</div>
