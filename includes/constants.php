50339ec58a8a75129574bb08804e0bbd1d14cf6e
Patrick Barnhardt <contact@patrickbarnhardt.info>
2023-12-12 15:57:21 -0500

    Update constants.php

--- includes/constants.php
+++ includes/constants.php
@@ -13,3 +13,213 @@ define('JOBTYPES', array(
  * Define constant for date formats
  */
 define('DATE_FORMAT', 'm/d/Y');
+
+/**
+ * define the States array
+ */
+define('STATES', array(
+    array(
+        "value" => "AL",
+        "label" => "Alabama"
+    ),
+    array(
+        "value" => "AK",
+        "label" => "Alaska"
+    ),
+    array(
+        "value" => "AZ",
+        "label" => "Arizona"
+    ),
+    array(
+        "value" => "AR",
+        "label" => "Arkansas"
+    ),
+    array(
+        "value" => "CA",
+        "label" => "California"
+    ),
+    array(
+        "value" => "CO",
+        "label" => "Colorado"
+    ),
+    array(
+        "value" => "CT",
+        "label" => "Connecticut"
+    ),
+    array(
+        "value" => "DE",
+        "label" => "Delaware"
+    ),
+    array(
+        "value" => "DC",
+        "label" => "District Of Columbia"
+    ),
+    array(
+        "value" => "FL",
+        "label" => "Florida"
+    ),
+    array(
+        "value" => "GA",
+        "label" => "Georgia"
+    ),
+    array(
+        "value" => "HI",
+        "label" => "Hawaii"
+    ),
+    array(
+        "value" => "ID",
+        "label" => "Idaho"
+    ),
+    array(
+        "value" => "IL",
+        "label" => "Illinois"
+    ),
+    array(
+        "value" => "IN",
+        "label" => "Indiana"
+    ),
+    array(
+        "value" => "IA",
+        "label" => "Iowa"
+    ),
+    array(
+        "value" => "KS",
+        "label" => "Kansas"
+    ),
+    array(
+        "value" => "KY",
+        "label" => "Kentucky"
+    ),
+    array(
+        "value" => "LA",
+        "label" => "Louisiana"
+    ),
+    array(
+        "value" => "ME",
+        "label" => "Maine"
+    ),
+    array(
+        "value" => "MD",
+        "label" => "Maryland"
+    ),
+    array(
+        "value" => "MA",
+        "label" => "Massachusetts"
+    ),
+    array(
+        "value" => "MI",
+        "label" => "Michigan"
+    ),
+    array(
+        "value" => "MN",
+        "label" => "Minnesota"
+    ),
+    array(
+        "value" => "MS",
+        "label" => "Mississippi"
+    ),
+    array(
+        "value" => "MO",
+        "label" => "Missouri"
+    ),
+    array(
+        "value" => "MT",
+        "label" => "Montana"
+    ),
+    array(
+        "value" => "NE",
+        "label" => "Nebraska"
+    ),
+    array(
+        "value" => "NV",
+        "label" => "Nevada"
+    ),
+    array(
+        "value" => "NH",
+        "label" => "New Hampshire"
+    ),
+    array(
+        "value" => "NJ",
+        "label" => "New Jersey"
+    ),
+    array(
+        "value" => "NM",
+        "label" => "New Mexico"
+    ),
+    array(
+        "value" => "NY",
+        "label" => "New York"
+    ),
+    array(
+        "value" => "NC",
+        "label" => "North Carolina"
+    ),
+    array(
+        "value" => "ND",
+        "label" => "North Dakota"
+    ),
+    array(
+        "value" => "OH",
+        "label" => "Ohio"
+    ),
+    array(
+        "value" => "OK",
+        "label" => "Oklahoma"
+    ),
+    array(
+        "value" => "OR",
+        "label" => "Oregon"
+    ),
+    array(
+        "value" => "PA",
+        "label" => "Pennsylvania"
+    ),
+    array(
+        "value" => "RI",
+        "label" => "Rhode Island"
+    ),
+    array(
+        "value" => "SC",
+        "label" => "South Carolina"
+    ),
+    array(
+        "value" => "SD",
+        "label" => "South Dakota"
+    ),
+    array(
+        "value" => "TN",
+        "label" => "Tennessee"
+    ),
+    array(
+        "value" => "TX",
+        "label" => "Texas"
+    ),
+    array(
+        "value" => "UT",
+        "label" => "Utah"
+    ),
+    array(
+        "value" => "VT",
+        "label" => "Vermont"
+    ),
+    array(
+        "value" => "VA",
+        "label" => "Virginia"
+    ),
+    array(
+        "value" => "WA",
+        "label" => "Washington"
+    ),
+    array(
+        "value" => "WV",
+        "label" => "West Virginia"
+    ),
+    array(
+        "value" => "WI",
+        "label" => "Wisconsin"
+    ),
+    array(
+        "value" => "WY",
+        "label" => "Wyoming"
+    )
+));
