032355729c3dff561bf7d11d8c4aa6d790cc64b4
Patrick Barnhardt <contact@patrickbarnhardt.info>
2023-12-12 13:25:08 -0500

    Update sidebar.php

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -21,15 +21,19 @@
                     <div class="sb-nav-link-icon"><i class="fa-solid fa-school"></i></div>
                     Schools
                 </a>
-                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts"
+                    aria-expanded="false" aria-controls="collapseLayouts">
                     <div class="sb-nav-link-icon"><i class="fa-solid fa-graduation-cap"></i></div>
                     Degree Programs
                     <div class="side-nav-collapse-arrow"><i class="fa-solid fa-chevron-down"></i></div>
                 </a>
-                <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne"
+                    data-bs-parent="#sidenavAccordion">
                     <nav class="side-nav-menu-nested nav">
-                        <a class="nav-link" href="<?php echo APP_URL . '/admin/dashboard.php?view=majors&major=list' ?>">Majors</a>
-                        <a class="nav-link" href="<?php echo APP_URL . '/admin/dashboard.php?view=degrees&degree=list' ?>">Degrees</a>
+                        <a class="nav-link"
+                            href="<?php echo APP_URL . '/admin/dashboard.php?view=majors&major=list' ?>">Majors</a>
+                        <a class="nav-link"
+                            href="<?php echo APP_URL . '/admin/dashboard.php?view=degrees&degree=list' ?>">Degrees</a>
                     </nav>
                 </div>
                 <a class="nav-link" href="<?php echo APP_URL . '/admin/dashboard.php?view=jobs&job=list' ?>">
@@ -38,7 +42,7 @@
                 </a>
                 <a class="nav-link" href="<?php echo APP_URL . '/admin/dashboard.php?view=subjects&subject=list' ?>">
                     <div class="sb-nav-link-icon"><i class="fa-solid fa-book"></i></div>
-                    Subjects/Areas of Study
+                    Subjects/Fields
                 </a>
                 <a class="nav-link" href="<?php echo APP_URL . '/admin/dashboard.php?view=users&user=list' ?>">
                     <div class="sb-nav-link-icon"><i class="fa-solid fa-users"></i></div>
