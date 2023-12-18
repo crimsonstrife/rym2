61d5f7bec532cd41ba5866cffd481fd29b099d76
Patrick Barnhardt <contact@patrickbarnhardt.info>
2023-12-18 13:27:25 -0500

    Update settings_content.php

---
+++ admin/settings_content.php
@@ -0,0 +1,17 @@
+<?php
+//Prevent direct access to this file by checking if the constant ISVALIDUSER is defined.
+if (!defined('ISVALIDUSER')) {
+    die('Error: Invalid request');
+} // idea from https://stackoverflow.com/a/409515 (user UnkwnTech)
+?>
+<!-- main content -->
+<div id="layout_content">
+    <main>
+        <div class="container-fluid px-4">
+            <h1 class="mt-4">Settings</h1>
+            <div class="row">
+                <!-- Settings Form -->
+            </div>
+        </div>
+    </main>
+</div>
