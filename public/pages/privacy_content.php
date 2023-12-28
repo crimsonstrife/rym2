522ddf2715b59dcbc5cb8cea621166eb095db9eb
Patrick Barnhardt <contact@patrickbarnhardt.info>
2023-12-28 15:44:37 -0500

    Update privacy_content.php

---
+++ public/pages/privacy_content.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This is the content for the privacy policy page of the website
+ * it uses the markdown format to display the content
+ *
+ * Author:  Patrick Barnhardt
+ * Author Email: pbarnh1@wgu.edu
+ * Date:   12/28/2023
+ *
+ * @package RYM2
+ * Filename: privacy_content.php
+ * @version 1.0.0
+ * @requires PHP 7.2.5+
+ * @requires Bootstrap 5.3.2+
+ * @requires Font Awesome 6.4.2+
+ * @requires jQuery 3.7.1+
+ */
+
+//include the header
+include_once('header.php');
+?>
+
+<?php include_once('footer.php'); ?>
