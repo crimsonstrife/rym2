184950714708422f91acdb045a3af947af17c1d2
Patrick Barnhardt <contact@patrickbarnhardt.info>
2024-01-04 14:31:59 -0500

    Update dashboard.php
    added validation to check user for a role that is allowed to view the dashboard.

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -10,6 +10,15 @@ define('CAN_INCLUDE', true); // Define a constant to control access to the inclu
 // Include config file
 require_once(__DIR__ . '../../config/app.php');
 
+//include the permissions class
+$permissionsObject = new Permission();
+
+//auth class
+$auth = new Authenticator();
+
+//user class
+$user = new User();
+
 //check post request for login status and uid.
 if (isset($_GET['login']) && isset($_GET['u'])) {
     switch ($_GET['login']) {
@@ -40,69 +49,81 @@ if (isset($_SESSION['user_id'])) {
     // Define a constant to control the access of the include files
     define('ISVALIDUSER', true); // idea from https://stackoverflow.com/a/409515 (user UnkwnTech)
 
-    //include the header
-    include_once('./header.php');
+    //get the id of the view dashboard permission
+    $viewDashboardPermissionId = $permissionsObject->getPermissionIdByName('VIEW DASHBOARD');
+
+    //boolean to check if the user has the view dashboard permission
+    $hasViewDashboardPermission = $auth->checkUserPermission(intval($_SESSION['user_id']), $viewDashboardPermissionId);
+
+    //if the user does not have the view dashboard permission, prevent access to the dashboard
+    if (!$hasViewDashboardPermission) {
+        //die with an error message
+        die('Error: You do not have permission to access the dashboard, contact the Administrator.');
+    } else {
+        //include the header
+        include_once('./header.php');
 ?>
 <div id="layout">
     <?php
-    //include the sidebar
-    include_once('./sidebar.php');
+        //include the sidebar
+        include_once('./sidebar.php');
 
-    //switch content based on the parameter in the url
-    if (isset($_GET['view'])) {
-        switch ($_GET['view']) {
-            case 'events':
-                include_once('content/events_content.php');
-                break;
-            case 'schools':
-                include_once('content/schools_content.php');
-                break;
-            case 'majors':
-                include_once('content/majors_content.php');
-                break;
-            case 'degrees':
-                include_once('content/degrees_content.php');
-                break;
-            case 'users':
-                include_once('content/users_content.php');
-                break;
-            case 'roles':
-                include_once('content/roles_content.php');
-                break;
-            case 'settings':
-                include_once('content/settings_content.php');
-                break;
-            case 'jobs':
-                include_once('content/jobs_content.php');
-                break;
-            case 'subjects':
-                include_once('content/subjects_content.php');
-                break;
-            case 'reports':
-                include_once('content/reports_content.php');
-                break;
-            case 'students':
-                include_once('content/students_content.php');
-                break;
-            case 'contact-log':
-                include_once('content/contactlog_content.php');
-                break;
-            case 'activity-log':
-                include_once('content/activitylog_content.php');
-                break;
-            case 'search':
-                include_once('content/search.php');
-                break;
-            default:
-                include_once('content/admin_content.php');
-                break;
+        //switch content based on the parameter in the url
+        if (isset($_GET['view'])) {
+            switch ($_GET['view']) {
+                case 'events':
+                    include_once('content/events_content.php');
+                    break;
+                case 'schools':
+                    include_once('content/schools_content.php');
+                    break;
+                case 'majors':
+                    include_once('content/majors_content.php');
+                    break;
+                case 'degrees':
+                    include_once('content/degrees_content.php');
+                    break;
+                case 'users':
+                    include_once('content/users_content.php');
+                    break;
+                case 'roles':
+                    include_once('content/roles_content.php');
+                    break;
+                case 'settings':
+                    include_once('content/settings_content.php');
+                    break;
+                case 'jobs':
+                    include_once('content/jobs_content.php');
+                    break;
+                case 'subjects':
+                    include_once('content/subjects_content.php');
+                    break;
+                case 'reports':
+                    include_once('content/reports_content.php');
+                    break;
+                case 'students':
+                    include_once('content/students_content.php');
+                    break;
+                case 'contact-log':
+                    include_once('content/contactlog_content.php');
+                    break;
+                case 'activity-log':
+                    include_once('content/activitylog_content.php');
+                    break;
+                case 'search':
+                    include_once('content/search.php');
+                    break;
+                default:
+                    include_once('content/admin_content.php');
+                    break;
+            }
+        } else {
+            include_once('content/admin_content.php');
         }
-    } else {
-        include_once('content/admin_content.php');
-    }
 
-    //include the footer
-    include_once('./footer.php');
+        //include the footer
+        include_once('./footer.php');
+    }
 } else {
     performRedirect('/login.php');
 }
