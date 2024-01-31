160d44d42d527e77902d9f35cb8148e0efc6999c
Patrick Barnhardt <contact@patrickbarnhardt.info>
2024-01-31 15:41:12 -0500

    Update contactlog_content.php

--- admin/content/contactlog_content.php
+++ admin/content/contactlog_content.php
@@ -48,7 +48,7 @@ if (!isset($hasViewDashboardPermission)) {
         } else {
 ?>
             <!-- main content -->
-            <div id="layout_content">
+            <div id="layout_content" class="w-95 mx-auto">
                 <main>
                     <div class="container-fluid px-4">
                         <h1 class="mt-4">Contact Log</h1>
