c73e0c8988bd570f678ae53092e2938af36ca954
Patrick Barnhardt <contact@patrickbarnhardt.info>
2024-01-31 15:41:49 -0500

    Update subjects_content.php

--- admin/content/subjects_content.php
+++ admin/content/subjects_content.php
@@ -24,10 +24,10 @@ if (!isset($hasViewDashboardPermission)) {
         //include the error message file
         include_once(__DIR__ . '/../../includes/errors/errorMessage.inc.php');
     } else { ?>
-<!-- main content -->
-<div id="layout_content">
-    <main>
-        <?php
+        <!-- main content -->
+        <div id="layout_content" class="w-95 mx-auto">
+            <main>
+                <?php
                 if (isset($_GET['subject'])) {
                     switch ($_GET['subject']) {
                         case 'list':
@@ -91,8 +91,8 @@ if (!isset($hasViewDashboardPermission)) {
                     include_once('./view/list/subject_list.php');
                 }
                 ?>
-    </main>
-</div>
+            </main>
+        </div>
 <?php
     }
 } ?>
