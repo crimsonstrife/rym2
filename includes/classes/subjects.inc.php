0d3d86ccfb2173b05b19448b77215802589e22ea
Patrick Barnhardt <contact@patrickbarnhardt.info>
2024-02-21 15:43:34 -0500

    Update subjects.inc.php

--- includes/classes/subjects.inc.php
+++ includes/classes/subjects.inc.php
@@ -21,30 +21,30 @@ declare(strict_types=1); // Forces PHP to adhere to strict typing, if types do n
  */
 abstract class Subject
 {
-    public int $aoi_id; //id from the areas of interest table
+    public int $subjectID; //id from the areas of interest table
 
-    public string $aoi_name; //name of the subject
+    public string $subjectName; //name of the subject
 
-    public string $created_at; //created_at date
+    public string $subjectCreateDate; //created_at date
 
-    public string $updated_at; //updated_at date
+    public string $subjectUpdateDate; //updated_at date
 
-    public int $created_by; //id from the users table
+    public int $subjectCreateUser; //id from the users table
 
-    public int $updated_by; //id from the users table
+    public int $subjectUpdateUser; //id from the users table
 
     /**
      * Subjects constructor.
      *
      */
-    public function __construct($id, $name, $created_at, string $updated_at, $created_by, $updated_by)
+    public function __construct(int $aoiID, string $aoiName, string $createdAt, string $updatedAt, int $createdBy, int $updatedBy)
     {
-        $this->aoi_id = $id;
-        $this->aoi_name = $name;
-        $this->created_at = $created_at;
-        $this->updated_at = $updated_at;
-        $this->created_by = $created_by;
-        $this->updated_by = $updated_by;
+        $this->subjectID = $aoiID;
+        $this->subjectName = $aoiName;
+        $this->subjectCreateDate = $createdAt;
+        $this->subjectUpdateDate = $updatedAt;
+        $this->subjectCreateUser = $createdBy;
+        $this->subjectUpdateUser = $updatedBy;
     }
 
     /**
@@ -57,77 +57,77 @@ abstract class Subject
     /**
      * Get a single subject from the database
      *
-     * @param int $aoi_id //id from the areas of interest table
+     * @param int $aoiID //id from the areas of interest table
      * @return array
      */
-    abstract public function getSubject(int $aoi_id): array;
+    abstract public function getSubject(int $aoiID): array;
 
     /**
      * Add a subject to the database
      *
-     * @param string $aoi_name //name of the subject
-     * @param int $user_id //id from the users table *optional
+     * @param string $aoiName //name of the subject
+     * @param int $userID //id from the users table *optional
      * @return bool
      */
-    abstract public function addSubject(string $aoi_name, int $user_id): bool;
+    abstract public function addSubject(string $aoiName, int $userID): bool;
 
     /**
      * Update a subject in the database
      *
-     * @param int $aoi_id //id from the areas of interest table
-     * @param string $aoi_name //name of the subject
-     * @param int $user_id //id from the users table *optional
+     * @param int $aoiID //id from the areas of interest table
+     * @param string $aoiName //name of the subject
+     * @param int $userID //id from the users table *optional
      * @return bool
      */
-    abstract public function updateSubject(int $aoi_id, string $aoi_name, int $user_id): bool;
+    abstract public function updateSubject(int $aoiID, string $aoiName, int $userID): bool;
 
     /**
      * Delete a subject from the database
      *
-     * @param int $aoi_id //id from the areas of interest table
+     * @param int $aoiID //id from the areas of interest table
      * @return bool
      */
-    abstract public function deleteSubject(int $aoi_id): bool;
+    abstract public function deleteSubject(int $aoiID): bool;
 
     /**
      * Get subject name by id
      *
-     * @param int $aoi_id //id from the areas of interest table
+     * @param int $aoiID //id from the areas of interest table
      * @return string
      */
-    abstract public function getSubjectName(int $aoi_id): string;
+    abstract public function getSubjectName(int $aoiID): string;
 
     /**
      * Get the created date of a subject
      *
-     * @param int $aoi_id //id from the areas of interest table
+     * @param int $aoiID //id from the areas of interest table
      * @return string
      */
-    abstract public function getSubjectCreatedDate(int $aoi_id): string;
+    abstract public function getSubjectCreatedDate(int $aoiID): string;
 
     /**
      * Get the last updated date of a subject
      *
-     * @param int $aoi_id //id from the areas of interest table
+     * @param int $aoiID //id from the areas of interest table
      * @return string
      */
-    abstract public function getSubjectLastUpdatedDate(int $aoi_id): string;
+    abstract public function getSubjectLastUpdatedDate(int $aoiID): string;
 
     /**
      * Get the created by user of a subject
      *
-     * @param int $aoi_id //id from the areas of interest table
+     * @param int $aoiID //id from the areas of interest table
      * @return User //user object
      */
-    abstract public function getSubjectCreatedBy(int $aoi_id): User;
+    abstract public function getSubjectCreatedBy(int $aoiID): User;
 
     /**
      * Get the last updated by user of a subject
      *
-     * @param int $aoi_id //id from the areas of interest table
+     * @param int $aoiID //id from the areas of interest table
      * @return User //user object
      */
-    abstract public function getSubjectLastUpdatedBy(int $aoi_id): User;
+    abstract public function getSubjectLastUpdatedBy(int $aoiID): User;
 
     /**
      * Get the total number of subjects in the database
@@ -139,26 +139,26 @@ abstract class Subject
     /**
      * Set the updated by user of a subject
      *
-     * @param int $aoi_id //id from the areas of interest table
-     * @param int $user_id //id from the users table
+     * @param int $aoiID //id from the areas of interest table
+     * @param int $userID //id from the users table
      * @return bool
      */
-    abstract public function setSubjectLastUpdatedBy(int $aoi_id, int $user_id): bool;
+    abstract public function setSubjectLastUpdatedBy(int $aoiID, int $userID): bool;
 
     /**
      * Set the created by user of a subject
      *
-     * @param int $aoi_id //id from the areas of interest table
-     * @param int $user_id //id from the users table
+     * @param int $aoiID //id from the areas of interest table
+     * @param int $userID //id from the users table
      * @return bool
      */
-    abstract public function setSubjectCreatedBy(int $aoi_id, int $user_id): bool;
+    abstract public function setSubjectCreatedBy(int $aoiID, int $userID): bool;
 
     /**
      * Check if a subject exists in the database
      *
-     * @param int $aoi_id //id from the areas of interest table
+     * @param int $aoiID //id from the areas of interest table
      * @return bool
      */
-    abstract public function subjectExists(int $aoi_id): bool;
+    abstract public function subjectExists(int $aoiID): bool;
 };
