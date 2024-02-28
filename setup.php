86c298dc8dca51020152d8ca3d3f21303ccfb509
Patrick Barnhardt <contact@patrickbarnhardt.info>
2024-02-28 18:22:08 -0500

    Update setup.php

--- setup.php
+++ setup.php
@@ -389,27 +389,18 @@ if (!function_exists('mail')) {
                                         try {
                                             $mysqli = connectToDatabase($_ENV['DB_HOST'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'], $_ENV['DB_DATABASE'], $PORT);
 
-                                            // Temporary variable, used to store current query
-                                            $temp = '';
-                                            // Read in entire file
-                                            $lines = file($sqlFile);
-                                            // Loop through each line
-                                            foreach ($lines as $line) {
-                                                // Skip it if it's a comment
-                                                if (substr($line, 0, 2) == '--' || $line == '')
-                                                continue;
+                                            //get the sql file
+                                            $sql = file_get_contents($sqlFile);
 
-                                                // Add this line to the current segment
-                                                $templine .= $line;
-                                                // If it has a semicolon at the end, it's the end of the query
-                                                if (substr(trim($line), -1, 1) == ';') {
-                                                    // Perform the query
-                                                    $mysqli->query($templine) or print('Error performing query \'<strong>' . $templine . '\': ' . $mysqli->error . '<br /><br />');
-                                                    // Reset temp variable to empty
-                                                    $temp = '';
-                                                }
+                                            //run the sql file
+                                            if ($mysqli->multi_query($sql)) {
+                                                do {
+                                                    //store the result
+                                                    if ($result = $mysqli->store_result()) {
+                                                        $result->free();
+                                                    }
+                                                } while ($mysqli->more_results() && $mysqli->next_result());
                                             }
-                                            echo "Tables imported successfully";
 
                                             //close the connection
                                             closeDatabaseConnection($mysqli);
