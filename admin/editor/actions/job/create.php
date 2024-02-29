58c90bd706dd363c147e894a98c950df7b6e7601
Patrick Barnhardt <contact@patrickbarnhardt.info>
2024-02-29 11:46:18 -0500

    Update create.php

--- admin/editor/actions/job/create.php
+++ admin/editor/actions/job/create.php
@@ -136,7 +136,7 @@ if (!$hasPermission) {
     } ?>
     <!-- Completion page content -->
     <div class="container-fluid px-4">
-        <h1 class="mt-4"><?php echo $job_title; ?></h1>
+        <h1 class="mt-4"><?php echo htmlspecialchars($job_title); ?></h1>
         <div class="row">
             <div class="card mb-4">
                 <!-- show completion message -->
@@ -164,10 +164,10 @@ if (!$hasPermission) {
                             <?php
                             if ($action == 'create') {
                                 if ($jobCreated) {
-                                    echo '<p>The job: ' . $job_title . ' has been created.</p>';
+                                    echo '<p>The job: ' . htmlspecialchars($job_title) . ' has been created.</p>';
                                 } else {
                                     echo '<i class="fa-solid fa-circle-exclamation"></i>';
-                                    echo '<p>The job: ' . $job_title . ' could not be created.</p>';
+                                    echo '<p>The job: ' . htmlspecialchars($job_title) . ' could not be created.</p>';
                                 }
                             }
                             ?>
@@ -179,9 +179,9 @@ if (!$hasPermission) {
                             <?php
                             if ($action == 'create') {
                                 if (!$canCreate) {
-                                    echo '<p>The job: ' . $job_title . ' could not be created due to an error.</p>';
+                                    echo '<p>The job: ' . htmlspecialchars($job_title) . ' could not be created due to an error.</p>';
                                 } else {
-                                    echo '<p>The job: ' . $job_title . ' has been created.</p>';
+                                    echo '<p>The job: ' . htmlspecialchars($job_title) . ' has been created.</p>';
                                 }
                             }
                             ?>
@@ -195,7 +195,7 @@ if (!$hasPermission) {
                                 if ($action == 'create') {
                                     if ($jobCreated) {
                                         echo '<span><a href="' . APP_URL . '/admin/dashboard.php?view=jobs&job=list" class="btn btn-primary">Return to Job List</a></span>';
-                                        echo '<span><a href="' . APP_URL . '/admin/dashboard.php?view=jobs&job=single&id=' . $job_id . '" class="btn btn-secondary">Go to Job</a></span>';
+                                        echo '<span><a href="' . APP_URL . '/admin/dashboard.php?view=jobs&job=single&id=' . htmlspecialchars($job_id) . '" class="btn btn-secondary">Go to Job</a></span>';
                                     } else {
                                         echo '<span><a href="' . APP_URL . '/admin/dashboard.php?view=jobs&job=list" class="btn btn-primary">Return to Job List</a></span>';
                                     }
