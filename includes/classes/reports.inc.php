32798efa808187df76155c70e51d38de6e94bc7d
Patrick Barnhardt <contact@patrickbarnhardt.info>
2024-02-29 13:52:07 -0500

    Update reports.inc.php

--- includes/classes/reports.inc.php
+++ includes/classes/reports.inc.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * Reports Class file for the College Recruitment Application
+ * This class is used to manage reports for the application and store them in the database
+ *
+ * Author:  Patrick Barnhardt
+ * Author Email: pbarnh1@wgu.edu
+ * Date:   10/16/2023
+ *
+ * @package RYM2
+ * Filename: reports.inc.php
+ * @version 1.0.0
+ * @requires PHP 8.1.2+
+ */
 
 declare(strict_types=1); // Forces PHP to adhere to strict typing, if types do not match an error is thrown.
 
