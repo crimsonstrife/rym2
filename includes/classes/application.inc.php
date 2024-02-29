1d699be0c2c07f842aa18bcc0933f6990f5f438e
Patrick Barnhardt <contact@patrickbarnhardt.info>
2024-02-29 13:51:18 -0500

    Update application.inc.php

--- includes/classes/application.inc.php
+++ includes/classes/application.inc.php
@@ -1,4 +1,18 @@
 <?php
+/**
+ * Application Class file for the College Recruitment Application
+ * This class is used to manage settings and features for the application
+ *
+ * Author:  Patrick Barnhardt
+ * Author Email: pbarnh1@wgu.edu
+ * Date:   10/16/2023
+ *
+ * @category Application
+ * @package RYM2
+ * Filename: application.inc.php
+ * @version 1.0.0
+ * @requires PHP 8.1.2+
+ */
 
 declare(strict_types=1); // Forces PHP to adhere to strict typing, if types do not match an error is thrown.
 
