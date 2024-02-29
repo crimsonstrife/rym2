113efbb5d0a38d353dd424ba86c53aa61b58850a
Patrick Barnhardt <contact@patrickbarnhardt.info>
2024-02-29 13:53:09 -0500

    Update userlogin.inc.php

--- includes/classes/userlogin.inc.php
+++ includes/classes/userlogin.inc.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * User Login Class file for the College Recruitment Application
  * Contains all the functions for the User Login Class and handles all the user related tasks with the database.
@@ -11,7 +10,7 @@
  * @package RYM2
  * Filename: users.inc.php
  * @version 1.0.0
- * @requires PHP 7.2.5+
+ * @requires PHP 8.1.2+
  */
 
 declare(strict_types=1); // Forces PHP to adhere to strict typing, if types do not match an error is thrown.
