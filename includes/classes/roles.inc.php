430b5fc53095ee41fb3fe6f1c1b8af343f10c9d8
Patrick Barnhardt <contact@patrickbarnhardt.info>
2024-02-29 13:52:14 -0500

    Update roles.inc.php

--- includes/classes/roles.inc.php
+++ includes/classes/roles.inc.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Roles Class file for the College Recruitment Application
  * Contains all the functions for the Roles Class and handles all user role functions.
@@ -11,7 +10,7 @@
  * @package RYM2
  * Filename: roles.inc.php
  * @version 1.0.0
- * @requires PHP 7.2.5+
+ * @requires PHP 8.1.2+
  */
 
 declare(strict_types=1); // Forces PHP to adhere to strict typing, if types do not match an error is thrown.
