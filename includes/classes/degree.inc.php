0bc4f61234e5dc8b2e6f4db978a2967887f60410
Patrick Barnhardt <contact@patrickbarnhardt.info>
2024-02-29 13:51:33 -0500

    Update degree.inc.php

--- includes/classes/degree.inc.php
+++ includes/classes/degree.inc.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Degree Class file for the College Recruitment Application
  * Contains all the functions for the Degree Class.
@@ -11,7 +10,7 @@
  * @package RYM2
  * Filename: degree.inc.php
  * @version 1.0.0
- * @requires PHP 7.2.5+
+ * @requires PHP 8.1.2+
  */
 
 declare(strict_types=1); // Forces PHP to adhere to strict typing, if types do not match an error is thrown.
