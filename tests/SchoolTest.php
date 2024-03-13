46675dc717cc7713ed200599317ba72e11c53221
Patrick Barnhardt <contact@patrickbarnhardt.info>
2024-03-12 22:28:59 -0400

    Create SchoolTest.php

---
+++ tests/SchoolTest.php
@@ -0,0 +1,62 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require_once 'includes/boostrap.php';
+
+/**
+ * Test case for School class
+ */
+class SchoolTest extends TestCase
+{
+
+    /**
+     * Test the createSchool method to add a school to the database
+     */
+    public function testCreateSchool()
+    {
+        $schoolClass = new School();
+        $name = 'Test School';
+        $address = '123 Main St';
+        $city = 'Anytown';
+        $state = 'NY';
+        $zipcode = '12345';
+        $createdBy = 1; //default created by to the first entry in the database
+        $expectedResult = true;
+
+        //create the school
+        $result = $schoolClass->createSchool($name, $address, $city, $state, $zipcode, $createdBy);
+
+        //check if the school was created
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    /**
+     * Test the getSchool methods to get a school from the database
+     */
+    public function testGetSchool()
+    {
+        $schoolClass = new School();
+        $expectedResult = 'Test School';
+
+        //get the school ID
+        $result = $schoolClass->getSchoolIdByName('Test School');
+
+        //check that the result is not empty, null or false
+        $this->assertNotEmpty($result);
+        $this->assertNotNull($result);
+
+        //set the ID
+        $id = $result;
+
+        //get the school by ID
+        $school = $schoolClass->getSchoolById($id);
+
+        //get the school name from the array
+        $result = $school['name'];
+
+        //check if the school was retrieved
+        $this->assertEquals($expectedResult, $result);
+    }
+
+}
