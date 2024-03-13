1abc21561f3c9291cba2bd8610acefd528dd30cb
Patrick Barnhardt <contact@patrickbarnhardt.info>
2024-03-12 22:42:46 -0400

    Update SchoolTest.php

--- tests/SchoolTest.php
+++ tests/SchoolTest.php
@@ -59,4 +59,60 @@ class SchoolTest extends TestCase
         $this->assertEquals($expectedResult, $result);
     }
 
+    /**
+     * Test the updateSchool method to update a school in the database
+     */
+    public function testUpdateSchool()
+    {
+        $schoolClass = new School();
+        //get the school ID for the test school
+        $result = $schoolClass->getSchoolIdByName('Test School');
+
+        //check that the result is not empty, null or false
+        $this->assertNotEmpty($result);
+        $this->assertNotNull($result);
+
+        //set the ID
+        $id = $result;
+
+        //set the updated school data
+        $name = 'Updated Test School';
+        $address = '456 Main St';
+        $city = 'Anytown';
+        $state = 'NY';
+        $zipcode = '57891';
+        $updatedBy = 1; //default updated by to the first entry in the database
+        $expectedResult = true;
+
+        //update the school
+        $result = $schoolClass->updateSchool($id, $name, $address, $city, $state, $zipcode, $updatedBy);
+
+        //check if the school was updated
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    /**
+     * Test the deleteSchool method to delete a school from the database
+     */
+    public function testDeleteSchool()
+    {
+        $schoolClass = new School();
+        //get the school ID for the updated test school
+        $result = $schoolClass->getSchoolIdByName('Updated Test School');
+
+        //check that the result is not empty, null or false
+        $this->assertNotEmpty($result);
+        $this->assertNotNull($result);
+
+        //set the ID
+        $id = $result;
+        $expectedResult = true;
+
+        //delete the school
+        $result = $schoolClass->deleteSchool($id);
+
+        //check if the school was deleted
+        $this->assertEquals($expectedResult, $result);
+    }
+
 }
